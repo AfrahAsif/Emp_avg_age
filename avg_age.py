2bf0fc45e6381284abffa11e589b2b9fb8343bf4
AfrahAsif <afrahasif@gmail.com>
2022-01-04 21:49:50 +0530

    Create avg_age.py

---
+++ avg_age.py
@@ -0,0 +1,15 @@
+class Employee:
+  def __init__(self,fname,lname,age):
+    self.fname= fname
+    self.lname=lname
+    self.age=age
+
+
+employees = [Employee('name1','name2',22),
+Employee('name1','name2',30),
+Employee('name1','name2',32),
+Employee('name1','name2',20),
+Employee('name1','name2',23)]
+
+ages = [Emp.age for Emp in employees]
+print('Average Age Is: {}'.format(sum(ages)/len(ages)))
